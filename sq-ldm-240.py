2b96dbb7aff4930c5a16564b3c841960af0ca7ec
Frossie Economou <frossie@lsst.org>
2015-05-19 12:13:08 -0500

    Added WBS

--- sq-ldm-240.py
+++ sq-ldm-240.py
@@ -21,21 +21,26 @@ project = jira.project('DLP')
 cycles = jirakit.cycles()
 if debug: print cycles
 
-milestones = jira.search_issues('project = DLP and issuetype = Milestone order by fixVersion DESC')
+milestones = jira.search_issues('project = DLP and issuetype = Milestone order by WBS ASC')
 
 table = [cycles]
 
 for milestone in milestones:
-    issue = jira.issue(milestone, fields = 'key, summary, fixVersions, customfield_10500')
+    issue = jira.issue(milestone)
     #    if debug: print milestone
-
+    print 40 * '*'
     # Get the release associated with this milestone
     if issue.fields.fixVersions:
         release = issue.fields.fixVersions[0]
         cyc = release.name
         milestonestr = milestone.key
+        if issue.fields.customfield_10500:
+            WBS =  issue.fields.customfield_10500
+        else:
+            WBS = None
+            
+        row = [WBS]
 
-        row = []
         for k in cycles:
             if k == cyc:
                 row.append(milestonestr)
