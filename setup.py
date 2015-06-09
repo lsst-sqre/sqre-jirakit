af62d4fbc99d4a9ce9e2a83c8949728b7fc41b42
Frossie Economou <frossie@lsst.org>
2015-06-08 17:51:09 -0700

    Re-organisation to support distribution

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+
 from setuptools import setup, find_packages
+
+
+setup(
+    name="jirakit",
+    version="0.5",
+    description="JIRA manipulation kit for LSST DM",
+    license="GPL",
+    author="LSST DM team",
+    author_email="dm-devel@lists.lsst.org",
+    url="https://github.com/lsst-sqre/sqre-jirakit",
+    packages=find_packages(),
+    scripts=['scripts/sq-ldm-240.py'],
+    install_requires=['jira', 'tabulate']
+)
