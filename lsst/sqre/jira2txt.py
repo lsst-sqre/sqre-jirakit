c7b0253ba284815587880630b39ed99160df03d8
John Swinbank <swinbank@princeton.edu>
2015-08-02 18:28:24 -0400

    jira2txt takes a list of issues as argument.
    Rather than a server and a query to run. Ultimately, this means we can pull
    the query logic outside the display routine, making it easier to add error
    handling, code reuse, etc.

--- lsst/sqre/jira2txt.py
+++ lsst/sqre/jira2txt.py
@@ -3,14 +3,14 @@ from __future__ import print_function
 import sys
 from csv import DictWriter
 from collections import OrderedDict
+from urlparse import urljoin
 
 from tabulate import tabulate
 from jira import JIRA
 
 from lsst.sqre.jirakit import cycles
 
-
-def jira2txt(server, query, output=sys.stdout, csv=False, show_key=True, show_title=False, show_url=True):
+def jira2txt(issues, output=sys.stdout, csv=False, show_key=True, show_title=False, url_base=''):
     def makeRow(wbs, cycles, blank=None):
         row = OrderedDict()
         row['WBS'] = wbs
@@ -18,10 +18,8 @@ def jira2txt(server, query, output=sys.stdout, csv=False, show_key=True, show_ti
             row[cycle] = blank
         return row
 
-    jira = JIRA(dict(server=server))
-
     table = []
-    for issue in jira.search_issues(query, maxResults=None):
+    for issue in issues:
         if not issue.fields.fixVersions:
             print('No release assigned to', issue.key, file=sys.stderr)
             continue
@@ -42,8 +40,8 @@ def jira2txt(server, query, output=sys.stdout, csv=False, show_key=True, show_ti
             row[cyc] = issue.key
 
         # In CSV mode we can include a URL to the actual issue
-        if csv and show_url:
-            row[cyc] = '=HYPERLINK("{}","{}")'.format(server + "/browse/" + issue.key, row[cyc])
+        if csv and url_base:
+            row[cyc] = '=HYPERLINK("{}","{}")'.format(urljoin(url_base, issue.key), row[cyc])
 
         table.append(row)
 
