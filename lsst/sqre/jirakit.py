a087f63a9b963b158f8b5765464de977cdaba6fa
Tim Jenness <tjenness@lsst.org>
2015-08-04 14:39:01 -0700

    Cycles need to go from S14 to S21

--- lsst/sqre/jirakit.py
+++ lsst/sqre/jirakit.py
@@ -7,8 +7,9 @@ import itertools
 
 SERVER="https://jira.lsstcorp.org/"
 
-def cycles(seasons=['W', 'S'], years=range(15,21)):
-    return ["%s%d" % (s, y) for y in years for s in seasons]
+def cycles(seasons=['W', 'S'], years=range(14,22)):
+    # S14 is a cycle but W14 is not
+    return ["%s%d" % (s, y) for y in years for s in seasons if not (s == 'W' and y == 14)]
 
 def build_query(issue_types, wbs):
     return 'project = DLP AND issuetype in (%s) AND wbs ~ "%s" ORDER BY wbs ASC, fixVersion DESC' % (", ".join(issue_types), wbs)
