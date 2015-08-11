37c8507aaa4c3cd52bb4bbf908cc8fd35d1fe698
Tim Jenness <tjenness@lsst.org>
2015-08-11 10:18:56 -0700

    Cycles is no longer an iterator

--- tests/test_jirakit.py
+++ tests/test_jirakit.py
@@ -14,7 +14,7 @@ class JiraKitTest(unittest.TestCase):
     def testCycle(self):
 
         c = jirakit.cycles()
-        self.assertEqual(next(c), 'S14')
+        self.assertEqual(c[0], 'S14')
 
 if __name__ == '__main__':
     unittest.main()
